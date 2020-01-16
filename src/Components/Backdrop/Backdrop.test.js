61b17124c0f00509f0aeefd7171b7074bb822210
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-15 22:03:47 -0500

    BackDrop smoke test created

---
+++ src/Components/Backdrop/Backdrop.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Backdrop from './Backdrop';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  //render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(
+      <Backdrop />
+    , div);
+
+  //clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
