77ecf09f04b536f1e1315497ab16785c5bf7102b
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-15 22:02:52 -0500

    created TestData for tests

---
+++ src/HelperFunctions/TestData.js
@@ -0,0 +1,362 @@
+export default {
+  products: [
+    {
+      shop_id: 1,
+      id: 1,
+      item: "Flip Flop 1",
+      price: "20.00",
+      description: "Classic water-resistant flip flops",
+      image_url: "flipflop-1.jpg"
+    },
+    {
+      shop_id: 1,
+      id: 2,
+      item: "Flip Flop 2",
+      price: "30.00",
+      description: "Classic water-resistant flip flops",
+      image_url: "flipflop-1.jpg"
+    },
+    {
+      shop_id: 1,
+      id: 3,
+      item: "Flip Flop 3",
+      price: "10.00",
+      description: "Classic water-resistant flip flops",
+      image_url: "flipflop-1.jpg"
+    },
+    {
+      shop_id: 1,
+      id: 4,
+      item: "Flip Flop 4",
+      price: "12.00",
+      description: "Classic water-resistant flip flops",
+      image_url: "flipflop-1.jpg"
+    },
+    {
+      shop_id: 1,
+      id: 5,
+      item: "Flip Flop 5",
+      price: "14.00",
+      description: "Classic water-resistant flip flops",
+      image_url: "flipflop-1.jpg"
+    },
+    {
+      shop_id: 1,
+      id: 6,
+      item: "Flip Flop 6",
+      price: "15.00",
+      description: "Classic water-resistant flip flops",
+      image_url: "flipflop-1.jpg"
+    },
+    {
+      shop_id: 1,
+      id: 7,
+      item: "Flip Flop 7",
+      price: "21.00",
+      description: "Classic water-resistant flip flops",
+      image_url: "flipflop-1.jpg"
+    },
+    {
+      shop_id: 2,
+      id: 8,
+      item: "Hot Blended Coffee",
+      price: "3.00",
+      description: "Green Mountain Coffee Roasters® Breakfast Blend Coffee",
+      image_url: "coffee-1.png"
+    },
+    {
+      shop_id: 2,
+      id: 9,
+      item: "Dark Magic",
+      price: "14.99",
+      description: "Green Mountain Coffee Roasters® Dark Magic® Coffee",
+      image_url: "coffee-2.png"
+    },
+    {
+      shop_id: 3,
+      id: 10,
+      item: "Tye-Dye",
+      price: "45.00",
+      description: "Tye-Dyed skateboard to do radical tricks",
+      image_url: "skateboard1.png"
+    },
+    {
+      shop_id: 3,
+      id: 11,
+      item: "Shark Donut",
+      price: "55.00",
+      description: "Shark Donut design from Superior Skateboards",
+      image_url: "skateboard2.png"
+    },
+    {
+      shop_id: 3,
+      id: 12,
+      item: "Pikachu",
+      price: "55.00",
+      description: "Pikachu inspired skateboard",
+      image_url: "skateboard3.png"
+    },
+    {
+      shop_id: 3,
+      id: 13,
+      item: "Grafitti Heart Wall Art",
+      price: "140.00",
+      description: "Beautiful art displayed across 3 skateboards",
+      image_url: "skateboard4.png"
+    },
+    {
+      shop_id: 3,
+      id: 14,
+      item: "Smiley Face",
+      price: "50.00",
+      description: "A board that puts a smile on your face",
+      image_url: "skateboard5.png"
+    },
+    {
+      shop_id: 4,
+      id: 15,
+      item: "Colorful Pickup Truck",
+      price: "10.00",
+      description: "A bright colorful pickup truck for kids ages 3+",
+      image_url: "toy1.png"
+    },
+    {
+      shop_id: 4,
+      id: 16,
+      item: "Fun for All Toy Bundle",
+      price: "35.00",
+      description:
+        "A collection of toys for endless amounts of fun for kids ages 3+",
+      image_url: "toy2.png"
+    },
+    {
+      shop_id: 4,
+      id: 17,
+      item: "Despicable Me",
+      price: "15.00",
+      description: "The all time favorite Despicable for kids ages 3+",
+      image_url: "toy3.png"
+    },
+    {
+      shop_id: 4,
+      id: 18,
+      item: "Colorful Stack Cups",
+      price: "10.00",
+      description:
+        "A bright colorful set of cups that you can stack for kids ages 1+",
+      image_url: "toy4.png"
+    },
+    {
+      shop_id: 4,
+      id: 19,
+      item: "CBP Red Motorcycle",
+      price: "100.00",
+      description: "An awesome ride for kids ages 5+, safety gear not included",
+      image_url: "toy5.png"
+    },
+    {
+      shop_id: 5,
+      id: 20,
+      item: "Natural Soap - Pink",
+      price: "20.00",
+      description: "All natural soap in Pink",
+      image_url: "soap1.png"
+    },
+    {
+      shop_id: 5,
+      id: 21,
+      item: "Natural Meant Soap",
+      price: "25.00",
+      description: "All natural Meant soap",
+      image_url: "soap2.png"
+    },
+    {
+      shop_id: 5,
+      id: 22,
+      item: "Natural Honey Sunflower Soap",
+      price: "30.00",
+      description: "All natural soap made from Honey and Sunflowers",
+      image_url: "soap3.png"
+    },
+    {
+      shop_id: 5,
+      id: 23,
+      item: "Natural PRE de PROVENCE Soap",
+      price: "35.00",
+      description: "All natural soap from France",
+      image_url: "soap4.png"
+    },
+    {
+      shop_id: 5,
+      id: 24,
+      item: "Natural PRE de PROVENCE Soap Bundle",
+      price: "200.00",
+      description: "All natural soap bundle from France",
+      image_url: "soap5.png"
+    },
+    {
+      shop_id: 6,
+      id: 25,
+      item: "Classic Bacon Cheeseburger",
+      price: "5.75",
+      description: "The good ol classic Bacon Cheeseburger",
+      image_url: "burger1.png"
+    },
+    {
+      shop_id: 6,
+      id: 26,
+      item: "Insanity Burger",
+      price: "10.75",
+      description:
+        "The onions are dressed with vinegar, the cheese is slightly tangy Red Leicester, the bacon and pickles are...bacon and pickles.",
+      image_url: "burger2.png"
+    },
+    {
+      shop_id: 6,
+      id: 27,
+      item: "Peanut Butter Burger",
+      price: "8.00",
+      description:
+        "Smear a dollop of peanut butter on a warm burger, or heat the peanut butter to a thick, liquid consistency to add a rich, peanuty flavor.",
+      image_url: "burger3.png"
+    },
+    {
+      shop_id: 6,
+      id: 28,
+      item: "Spicy Vegan Portobello Mexican Burger",
+      price: "10.50",
+      description:
+        "Banish weird fake patties for good with this delicious spicy vegan portobello mexican burger – full of rich, meaty flavor, tons of fiber, and just real, fresh foods.",
+      image_url: "burger4.png"
+    },
+    {
+      shop_id: 6,
+      id: 29,
+      item: "Aussie-style burger",
+      price: "9.00",
+      description:
+        "Hunger busting burgers topped with every topping imaginable.",
+      image_url: "burger5.png"
+    },
+    {
+      shop_id: 8,
+      id: 31,
+      item: "asdf",
+      price: "111.00",
+      description: "asdf",
+      image_url: "product.png"
+    }
+  ],
+  shops: [
+    {
+      id: 1,
+      login_id: 1,
+      shop_name: "flipflops USA",
+      address: "90 Kent Ave, East River State Park, Brooklyn, NY 11211",
+      description:
+        "Since 2004, we’ve been curating quality flip flops and casual footwear for the beach and beyond. Whatever your mood or style, you’ll find the product to match it.",
+      start_date: "2020-01-03T00:00:00.000Z",
+      end_date: "2020-01-10T00:00:00.000Z",
+      opening_time: "10:00:00",
+      closing_time: "22:00:00",
+      service_type: "clothing and accessories",
+      image_url: "flipflops.png"
+    },
+    {
+      id: 2,
+      login_id: 2,
+      shop_name: "tullys coffee",
+      address: "657C Gates St., Rialto, CA 92376",
+      description:
+        'This "slow and low" ideology is shared by our four handpicked artists, who believe in the craft and care of their work. Follow our journey from San Francisco to Seattle as we meet four artists who add their own special spark to our Tully’s camper.',
+      start_date: "2020-01-09T00:00:00.000Z",
+      end_date: "2020-01-11T00:00:00.000Z",
+      opening_time: "12:00:00",
+      closing_time: "18:00:00",
+      service_type: "food and drinks",
+      image_url: "tully.png"
+    },
+    {
+      id: 3,
+      login_id: 3,
+      shop_name: "custom boards",
+      address: "9556 James Dr., Romeoville, IL 60446",
+      description:
+        "Custom made skateboards has been selling beautifully designed boards for decoration or to even shred if you want. You can even come in with a design and we will try our best to replicate it.",
+      start_date: "2020-01-09T00:00:00.000Z",
+      end_date: "2020-01-11T00:00:00.000Z",
+      opening_time: "12:00:00",
+      closing_time: "18:00:00",
+      service_type: "sports and hobbies",
+      image_url: "customboards.png"
+    },
+    {
+      id: 4,
+      login_id: 4,
+      shop_name: "the toy barn",
+      address: "2 Arch Rd., Norman, OK 73072",
+      description:
+        "From custom made to store brand toys, discover toys you did not know existed!",
+      start_date: "2020-01-09T00:00:00.000Z",
+      end_date: "2020-01-11T00:00:00.000Z",
+      opening_time: "12:00:00",
+      closing_time: "18:00:00",
+      service_type: "toys and leisure",
+      image_url: "thetoybarn.png"
+    },
+    {
+      id: 5,
+      login_id: 5,
+      shop_name: "the soap shop",
+      address: "34 Circle Ave., San Lorenzo, CA 94580",
+      description:
+        "Custom made all natural soaps, containing many scents such as Lavender, Rosemary, Mangos, and more. Come find your perfect soap!",
+      start_date: "2020-01-09T00:00:00.000Z",
+      end_date: "2020-01-11T00:00:00.000Z",
+      opening_time: "12:00:00",
+      closing_time: "18:00:00",
+      service_type: "bath and body",
+      image_url: "thesoapshop.png"
+    },
+    {
+      id: 6,
+      login_id: 6,
+      shop_name: "burgerlicious",
+      address: "22 Howard Ave., Clinton, MD 20735",
+      description:
+        "Best burgers you can ever try, made with my family secret ingredients, the taste will leave you speechless!",
+      start_date: "2020-01-09T00:00:00.000Z",
+      end_date: "2020-01-11T00:00:00.000Z",
+      opening_time: "12:00:00",
+      closing_time: "18:00:00",
+      service_type: "food and drinks",
+      image_url: "burgerlicious.png"
+    },
+    {
+      id: 7,
+      login_id: 42,
+      shop_name: "Banh Bao",
+      address: "1230 Bond Street",
+      description: "sfafda",
+      start_date: "2020-01-10T00:00:00.000Z",
+      end_date: "2020-01-10T00:00:00.000Z",
+      opening_time: "10:00:00",
+      closing_time: "22:00:00",
+      service_type: "food and drinks",
+      image_url: "shop.png"
+    },
+    {
+      id: 8,
+      login_id: 44,
+      shop_name: "jeff shop name",
+      address: "jeff address",
+      description: "jeff desc",
+      start_date: "2021-01-13T00:00:00.000Z",
+      end_date: "2022-01-13T00:00:00.000Z",
+      opening_time: "10:00:00",
+      closing_time: "22:00:00",
+      service_type: "food and drinks",
+      image_url: "shop.png"
+    }
+  ]
+};
