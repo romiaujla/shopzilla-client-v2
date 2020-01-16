116a0562070def8a99fc3ffaf42c4814be651fe0
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-15 22:03:51 -0500

    App smoke test created

---
+++ src/Components/App/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
+//this is the test case
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  //render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    , div);
+
+  //clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
